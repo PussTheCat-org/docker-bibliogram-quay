0c39dee8b00ddb2a38d4d5403563134388cbc3a0
TheFrenchGhosty <TheFrenchGhosty@posteo.is>
2022-01-30 02:56:24 +0100

    Add a config.js to the repo

---
+++ config.js
@@ -0,0 +1,9 @@
+/*
+	Welcome to the config file!
+	Add keys here to override values from /src/lib/constants.js. Please look at that file for override recommendations.
+	This file should hopefully never be altered upstream.
+	You must restart Bibliogram to apply these changes.
+*/
+
+module.exports = {
+}
